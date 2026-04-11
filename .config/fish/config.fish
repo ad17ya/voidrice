c7af89b7d14598061831b8bc639e01c7f340ae20
Aditya Kshirsagar <kaditya.anil@gmail.com>
2026-04-11 16:19:47 +0530

    add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+alias vim nvim
+alias vi nvim
+alias v nvim
+alias e nvim
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
